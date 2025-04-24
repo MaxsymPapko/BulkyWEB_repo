0242e9e108ca5301798cb51c0ebc2fe2a2244cba
Maxim Papko <papkom50@gmail.com>
2025-04-24 20:40:51 +0300

    add a data annotation

--- BulkyWEB/Models/Category.cs
+++ BulkyWEB/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace BulkyWEB.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWEB.Models;
 
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
     public int DisplayOrder { get; set; }
 }
