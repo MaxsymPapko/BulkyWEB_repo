61c46c920de62068168868680cf8f5fd5cb61c67
Maxim Papko <papkom50@gmail.com>
2025-04-28 20:58:28 +0300

    add a EntityFramework tools for working with DB

--- BulkyWEB/Data/ApplicationDbContext.cs
+++ BulkyWEB/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ namespace BulkyWEB.Data;
 
 public class ApplicationDbContext:DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
     {
         
     }
